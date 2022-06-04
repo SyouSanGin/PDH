f5e159c58bc4125a0e42f23da299c89bae730fdf
Mzxr <8059002+int127@user.noreply.gitee.com>
2022-06-04 05:50:15 +0000

    新建搜索模块 传入搜索词，返回参数数组，每个元素为字典，包括url、bvid、title、cid。

---
+++ search.py
@@ -0,0 +1,20 @@
+import re
+import requests
+def search(word):
+	url="https://api.bilibili.com/x/web-interface/search/type?keyword="+word+"&search_type=video"
+	headers={"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"}
+	data=requests.get(url=url,headers=headers).json()
+	need=data["data"]["result"]
+	next=list()
+	for i in need:
+		add={}
+		add["url"]=i["arcurl"]
+		add["bvid"]=i["bvid"]
+		add["title"]=i["title"]
+		page=requests.get(url=add["url"],headers=headers).text
+		cid=re.search("\"cid\":[0-9]+,",page)
+		add["cid"]=cid.group()[6:-1]
+		next.append(add)
+	return next
+
+print(search("中国科大"))
